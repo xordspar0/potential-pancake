3d18e262420bd0a43ab6427d31a379b0d46dc2ad
Jordan Christiansen <xordspar0@gmail.com>
2016-04-30 22:57:36 -0500

    Tweaked jump and attack animation transitions (see #2)

--- Player.lua
+++ Player.lua
@@ -87,7 +87,9 @@ function Player:input()
 	if self.onGround and self.controller:isDown("jump") then
 		self.onGround = false
 		self.yVelocity = self.jumpVelocity
-		self.character:setAnim("jump")
+		if self.character:getAnim() ~= "attack" then
+			self.character:setAnim("jump")
+		end
 	end
 	if self.onGround and self.character:getAnim() == "jump" then
 		self.character:setAnim("stand")
